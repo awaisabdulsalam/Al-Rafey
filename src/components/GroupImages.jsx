06eebcd181cbeb020b9fb4950f4844030d225cfe
Awais <theawais170@gmail.com>
2024-02-09 22:34:44 +0500

    fixing error

--- src/components/GroupImages.jsx
+++ src/components/GroupImages.jsx
@@ -1,13 +1,15 @@
+import group1 from "../assets/group1.png";
+
 const GroupImages = () => {
   return (
     <section className="flex gap-10 my-10 px-10">
       <div
         className="h-40 w-1/3 flex flex-col justify-center px-10"
-        style={{ backgroundImage: "url('/assets/group1.png')" }}
+        style={{ backgroundImage: `url(${group1})` }}
       >
         <h1 className="text-2xl">
-          Hanging <br />
-          <span className="font-bold">Lights hh</span>
+          Hanging<br />
+          <span className="font-bold">Lights</span>
         </h1>
         <div>
           <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">
