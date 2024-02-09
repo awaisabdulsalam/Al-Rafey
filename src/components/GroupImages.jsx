8acf0350432fcef3a60d75738612dabbfe44178b
Awais <theawais170@gmail.com>
2024-02-09 22:20:29 +0500

    fixing bg-images

--- src/components/GroupImages.jsx
+++ src/components/GroupImages.jsx
@@ -1,27 +1,50 @@
-
 const GroupImages = () => {
   return (
     <section className="flex gap-10 my-10 px-10">
-        <div className="h-40 w-1/3 flex flex-col justify-center px-10" style={{ backgroundImage: "url('/src/assets/group1.png')" }}>
-            <h1 className="text-2xl">Hanging <br /><span className="font-bold">Lights</span></h1>
-            <div>
-            <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">View Collections</button>
-            </div>
+      <div
+        className="h-40 w-1/3 flex flex-col justify-center px-10"
+        style={{ backgroundImage: "url('/assets/group1.png')" }}
+      >
+        <h1 className="text-2xl">
+          Hanging <br />
+          <span className="font-bold">Lights hh</span>
+        </h1>
+        <div>
+          <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">
+            View Collections
+          </button>
         </div>
-        <div className="h-40 w-1/3 flex flex-col justify-center px-10" style={{ backgroundImage: "url('/src/assets/group2.png')" }}>
-            <h1 className="text-2xl">Boomrang <br /><span className="font-bold">Chair</span></h1>
-            <div>
-            <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">View Collections</button>
-            </div>
+      </div>
+      <div
+        className="h-40 w-1/3 flex flex-col justify-center px-10"
+        style={{ backgroundImage: "url('/src/assets/group2.png')" }}
+      >
+        <h1 className="text-2xl">
+          Boomrang <br />
+          <span className="font-bold">Chair</span>
+        </h1>
+        <div>
+          <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">
+            View Collections
+          </button>
         </div>
-        <div className="h-40 w-1/3 flex flex-col justify-center px-10" style={{ backgroundImage: "url('/src/assets/group3.png')" }}>
-            <h1 className="text-2xl">Modern <br /><span className="font-bold">Chair</span></h1>
-            <div>
-            <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">View Collections</button>
-            </div>
+      </div>
+      <div
+        className="h-40 w-1/3 flex flex-col justify-center px-10"
+        style={{ backgroundImage: "url('/src/assets/group3.png')" }}
+      >
+        <h1 className="text-2xl">
+          Modern <br />
+          <span className="font-bold">Chair</span>
+        </h1>
+        <div>
+          <button className="border-2 border-grey-800 p-2 my-2 rounded-md text-gray-500">
+            View Collections
+          </button>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default GroupImages
+export default GroupImages;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/al-rafey/",
   plugins: [react()],
-  base: "/al-rafey/"
 })
