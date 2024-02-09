055a7464c62869968ef776b29d2188d664f78fed
Awais <theawais170@gmail.com>
2024-02-09 22:51:34 +0500

    fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,47 @@
-import heroImage from "../assets/heroimage.png"
+import heroImage from "../assets/heroimage.png";
 // import rectangleImage from "../assets/rectangle.png"
 // import ellipseImage from "../assets/ellipse.png"
+import vector1 from "../assets/vector1.png"
+import vector2 from "../assets/vector2.png"
 
 
 const Hero = () => {
   return (
     <>
-    <section>
+      <section>
         <div className="flex justify-between h-[80vh] bg-[#00D1BF] m-5 px-20 rounded-lg overflow-hidden">
-            <div className="flex flex-col justify-center">
-                <h1 className="text-6xl text-white my-5 font-bold">Flat Sale 50% off</h1>
-                <p className="text-2xl text-white my-2 font-semibold">All Executive in Rafey Traders</p>
-                <div>
-                <button className="bg-[#E5F43E] border-2 border-white px-8 py-3 my-1 rounded-lg text-lg">Shop Now</button>
-                </div>
+          <div className="flex flex-col justify-center">
+            <h1 className="text-6xl text-white my-5 font-bold">
+              Flat Sale 50% off
+            </h1>
+            <p className="text-2xl text-white my-2 font-semibold">
+              All Executive in Rafey Traders
+            </p>
+            <div>
+              <button className="bg-[#E5F43E] border-2 border-white px-8 py-3 my-1 rounded-lg text-lg">
+                Shop Now
+              </button>
             </div>
-            <div >
-                <img src={heroImage} className="bg-contain bg-no-repeat bg-center h-[600px]" style={{ backgroundImage: "url('/src/assets/vector1.png'), url('/src/assets/vector2.png')" }} alt="" srcset="" />
-            </div>
+          </div>
+          <div>
+            <img
+              src={heroImage}
+              className="bg-contain bg-no-repeat bg-center h-[600px]"
+              style={{
+                backgroundImage:
+                  `url(${vector1}), url(${vector1})`,
+              }}
+              alt=""
+            />
+          </div>
         </div>
         {/* <div className="flex mt-[-10] justify-center">
                 <img src={rectangleImage} alt="" srcset="" className="h-5" />
                 <img src={ellipseImage} alt="" srcset="" />
         </div> */}
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
