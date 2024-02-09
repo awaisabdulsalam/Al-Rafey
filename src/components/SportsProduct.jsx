ba62bd02db4f059f2f6fd0cd448262938df6faad
Awais <theawais170@gmail.com>
2024-02-09 23:00:50 +0500

    bg-img

--- src/components/SportsProduct.jsx
+++ src/components/SportsProduct.jsx
@@ -6,11 +6,11 @@ const SportsProduct = () => {
   return (
     <section className="bg-[#efeaf6] py-10">
         <div className="flex justify-around">
-            <img src={sports1} alt=""  />
+        <img src={sports1} alt="" srcSet="" />
             <img src={sports2} className="bg-no-repeat bg-bottom" style={{ backgroundImage: `url(${blueRectangle})` }} alt="" srcSet="" />
             <img src={sports1} alt="" srcSet="" />
             <img src={sports2} className="bg-no-repeat bg-bottom" style={{ backgroundImage: `url(${blueRectangle})`}} alt="" srcSet="" />
-            <img src={sports1} alt="" />
+            <img src={sports1} alt="" srcSet="" />
         </div>
     </section>
   )
