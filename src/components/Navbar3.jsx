e02c9ac70987ba5ee4b68495446ad7d2941c9458
Awais <theawais170@gmail.com>
2024-03-05 00:03:14 +0500

    added

--- src/components/Navbar3.jsx
+++ src/components/Navbar3.jsx
@@ -15,9 +15,7 @@ const Navbar3 = () => {
         <li className='border-[#a2a2a2] md:border-r-[1px] px-5 text-[18px]'><a href="#tech" className='sm:text-[12px] text-[#757575] md:text-[16px] hover:text-[#a6a6a6]'>Tech</a></li>
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#men" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Men</a></li>
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#women" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Women</a></li>
-        <Link to="al-rafey/categories">
-        <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Categories</a></li>
-        </Link>
+        
       </ul>
     </div>
   </div>
