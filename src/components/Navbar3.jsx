58ad4d7eb00f37587d039a5d8777090cf004ea03
Awais <theawais170@gmail.com>
2024-03-05 00:02:36 +0500

    ..

--- src/components/Navbar3.jsx
+++ src/components/Navbar3.jsx
@@ -16,6 +16,7 @@ const Navbar3 = () => {
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#men" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Men</a></li>
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#women" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Women</a></li>
         <Link to="al-rafey/categories">
+        <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Categories</a></li>
         </Link>
       </ul>
     </div>
