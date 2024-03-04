615b81e76bb32da967fa6a4a69908da96d619641
Awais <theawais170@gmail.com>
2024-03-05 00:01:28 +0500

    Categories added

--- src/components/Navbar3.jsx
+++ src/components/Navbar3.jsx
@@ -1,4 +1,5 @@
 import { TfiMenuAlt } from "react-icons/tfi";
+import { Link } from "react-router-dom";
 
 const Navbar3 = () => {
   return (
@@ -14,8 +15,9 @@ const Navbar3 = () => {
         <li className='border-[#a2a2a2] md:border-r-[1px] px-5 text-[18px]'><a href="#tech" className='sm:text-[12px] text-[#757575] md:text-[16px] hover:text-[#a6a6a6]'>Tech</a></li>
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#men" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Men</a></li>
         <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#women" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Women</a></li>
-        <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="#children" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Children</a></li>
-        <li className='md:border-l-[1px] md:border-[#a2a2a2] sm:border-l-0 px-5 text-[18px]'><a href="#grocery" className='sm:text-[12px] text-[#757575] md:text-[16px] hover:text-[#a6a6a6]'>Grocery</a></li>
+        <Link to="al-rafey/categories">
+        <li className='border-l-[1px] md:border-r-[1px] md:border-[#a2a2a2] px-5 text-[18px]'><a href="" className='sm:text-[12px] md:text-[16px] text-[#757575] hover:text-[#a6a6a6]'>Categories</a></li>
+        </Link>
       </ul>
     </div>
   </div>
