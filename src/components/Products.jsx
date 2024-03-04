708287c4d478bd38583d5af245e3e25fbafdc9aa
Awais <theawais170@gmail.com>
2024-03-05 00:16:52 +0500

    ..

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,7 @@ const Products = ({ products, heading, bgColor, color, id, btnColor }) => {
 
   return (
     <>
-    <section className="sm:py-5 sm:px-5 md:py-5 md:px-10 my-5" id={`${id}`}  style={{ backgroundColor: bgColor }}>
+    <section className="w-full sm:py-5 sm:px-5 md:py-5 md:px-10 my-5" id={`${id}`}  style={{ backgroundColor: bgColor }}>
       <div className="flex justify-between items-center border-b-[1px] border-gray-200">
         <h1 className={`product_heading text-[${color}] py-2 text-3xl border-b-[3px] border-[#4cd0e0]`}>
           {heading}
@@ -23,10 +23,10 @@ const Products = ({ products, heading, bgColor, color, id, btnColor }) => {
       </div>
 
       {/************    Product Cart   **************/}
-      <div className={`text-[${bgColor}] flex flex-wrap max-w-[100%] gap-4 py-10 px-2`}>
+      <div className={`text-[${bgColor}] flex justify-start flex-wrap max-w-[100%] gap-4 py-10 px-2`}>
       {
       products.map((product, index) => (
-        <div key={index} className={`sm:min-h-fit relative bg-[#fff] cursor-pointer border-2 border-gray-200 md:h-[400px] min-w-[200px] max-w-[250px] flex-1 py-3 px-3 mx-auto rounded-md overflow-hidden`}>
+        <div key={index} className={`sm:min-h-fit relative bg-[#fff] cursor-pointer border-2 border-gray-200 md:h-[400px] min-w-[200px] max-w-[250px] flex-1 py-3 px-3 rounded-md overflow-hidden`}>
           <div className={`text-[${color}] relative`}>
             <div className="cursor-pointer">
             <TbHeart onClick={() => {
